02957bfa8235c404b88e1db318d95b583290670d
Ollie Stevenson <ollie@stevenson.digital>
2020-06-05 22:51:19 +0100

    [add] scss + grid system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <h1>Tech Test 5874</h1>
-      <p>Testing Netlify CD</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Tech Test 5874</h1>
+            <p>Testing Netlify CD</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
