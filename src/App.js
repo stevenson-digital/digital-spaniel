956f1bf98df7d83d4edfa360301d33a3e3350dae
Ollie Stevenson <ollie@stevenson.digital>
2020-06-06 16:59:44 +0100

    [WIP] BurgerMenu layout + styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.scss'
 import Navbar from './components/Navbar'
+import BurgerMenu from './components/BurgerMenu'
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<BurgerMenu />
 			<div style={{height: '100vh', background: 'red'}} />
 			<div style={{height: '100vh', background: 'green'}} />
 			<div style={{height: '100vh', background: 'blue'}} />
--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
+import { NAV_LINKS } from '../constants/navigation'
 
 const BurgerMenu = () => {
 	const [isOpen, setIsOpen] = useState(false)
 
 	return (
 		<div className="BurgerMenu">
-			<h1>Hello world</h1>
+			<button className="BurgerMenu__close u-btn-clear">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" version="1.1" x="0px" y="0px"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g fill-rule="nonzero"><rect x="0" y="0" width="20" height="20"/><path d="M13.4142136,12 L20.4852814,19.0710678 L19.0710678,20.4852814 L12,13.4142136 L4.92893219,20.4852814 L3.51471863,19.0710678 L10.5857864,12 L3.51471863,4.92893219 L4.92893219,3.51471863 L12,10.5857864 L19.0710678,3.51471863 L20.4852814,4.92893219 L13.4142136,12 Z" fill="#000000" /></g></g></svg>
+			</button>
+			<ul className="BurgerMenu__links u-list-clear">
+				{NAV_LINKS.map((link) => {
+					return (
+						<li className="BurgerMenu__link" key={'burger-li-' + link.linkText}>
+							<a className="f-agg-48-36" href={link.link}>
+								{link.linkText}
+							</a>
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	)
 }
