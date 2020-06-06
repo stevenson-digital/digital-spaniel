74651e53f4a4b658d26bb420b6c544350e670003
Ollie Stevenson <ollie@stevenson.digital>
2020-06-06 08:19:59 +0100

    [add] eslint + prettier

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react'
 import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-sm-8 col-md-7 col-lg-6">
-            <h1 className="f-test-font-2">Tech Test 5874</h1>
-            <p>Testing Netlify CD</p>
-          </div>
-          <div className="col-xs-12 col-sm-4 col-md-5 col-lg-6">
-            <p>Two block grid test</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-12 col-sm-8 col-md-7 col-lg-6">
+						<h1 className="f-test-font-2">Tech Test 5874</h1>
+						<p>Testing Netlify CD</p>
+					</div>
+					<div className="col-xs-12 col-sm-4 col-md-5 col-lg-6">
+						<p>Two block grid test</p>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
