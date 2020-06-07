0af15687e5782c7bca41d41bfa221ba24e62ab0e
Ollie Stevenson <ollie@stevenson.digital>
2020-06-07 20:48:37 +0100

    [add] slide panel content changes on custom change slide event

--- src/components/CaseStudySlider/CaseStudySlider.js
+++ src/components/CaseStudySlider/CaseStudySlider.js
@@ -8,6 +8,14 @@ import 'slick-carousel/slick/slick-theme.css'
 export default class CaseStudySlider extends Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			currentIndex: 0,
+			currentHeading: SLIDES[0].heading,
+			currentDesc: SLIDES[0].desc,
+			currentLink: SLIDES[0].link,
+		}
+
 		this.next = this.next.bind(this)
 		this.previous = this.previous.bind(this)
 		this.handleChangeSlide = this.handleChangeSlide.bind(this)
@@ -22,11 +30,22 @@ export default class CaseStudySlider extends Component {
 	}
 
 	handleChangeSlide(direction) {
-		if (direction === 'next') {
-			this.next()
-		} else {
-			this.previous()
-		}
+		// Change slick slide
+		direction === 'next' ? this.next() : this.previous()
+
+		// Change text panel
+		const delay = direction === 'next' ? 500 : 300
+		setTimeout(() => {
+			this.setState({
+				currentHeading: SLIDES[this.state.currentIndex].heading,
+				currentDesc: SLIDES[this.state.currentIndex].desc,
+				currentLink: SLIDES[this.state.currentIndex].link,
+			})
+		}, delay)
+	}
+
+	handleUpdateCurrentIndex(current, next) {
+		this.setState({ currentIndex: next })
 	}
 
 	render() {
@@ -38,6 +57,7 @@ export default class CaseStudySlider extends Component {
 			slidesToScroll: 1,
 			cssEase: 'cubic-bezier(0.77, 0, 0.175, 1)',
 			speed: 700,
+			beforeChange: (current, next) => this.handleUpdateCurrentIndex(current, next),
 		}
 
 		return (
@@ -56,7 +76,12 @@ export default class CaseStudySlider extends Component {
 						)
 					})}
 				</Slider>
-				<CaseStudySliderPanel changeSlide={this.handleChangeSlide} />
+				<CaseStudySliderPanel
+					changeSlide={this.handleChangeSlide}
+					heading={this.state.currentHeading}
+					desc={this.state.currentDesc}
+					link={this.state.currentLink}
+				/>
 			</div>
 		)
 	}
