46ddf1f82709864c92c6bba5bcca651b585ec580
Ollie Stevenson <ollie@stevenson.digital>
2020-06-08 20:42:36 +0100

    [add] active filter state

--- src/components/RecentProjects/RecentProjectsFilters.js
+++ src/components/RecentProjects/RecentProjectsFilters.js
@@ -4,8 +4,12 @@ import { PROJECT_FILTERS } from '../../constants/projects'
 const RecentProjectsFilters = (props) => {
 	const { filterCallback } = props
 	const [positionModifier, setPositionModifier] = useState('position-0')
+	const [activeFilter, setActiveFilter] = useState(0)
 
 	const handleFilter = (filter, index) => {
+		// Set active filter
+		setActiveFilter(index)
+
 		// Move indicator
 		setPositionModifier('position-' + index)
 
@@ -24,7 +28,10 @@ const RecentProjectsFilters = (props) => {
 			{PROJECT_FILTERS.map((filter, index) => {
 				return (
 					<button
-						className="RecentProjectsFilters__btn f-os-regular-20-18 u-btn-clear active"
+						className={
+							'RecentProjectsFilters__btn f-os-regular-20-18 u-btn-clear' +
+							(activeFilter === index ? ' active' : '')
+						}
 						onClick={() => handleFilter(filter.slug, index)}
 						key={`filter-btn-${index}`}
 					>
