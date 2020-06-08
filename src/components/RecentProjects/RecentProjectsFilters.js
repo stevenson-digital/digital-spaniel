bb7c5d09c3d95be3f51ef6ba125ad567627a0b5c
Ollie Stevenson <ollie@stevenson.digital>
2020-06-08 20:36:54 +0100

    [add] desktop moving indicator project filters

--- src/components/RecentProjects/RecentProjectsFilters.js
+++ src/components/RecentProjects/RecentProjectsFilters.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PROJECT_FILTERS } from '../../constants/projects'
 
 const RecentProjectsFilters = (props) => {
 	const { filterCallback } = props
+	const [positionModifier, setPositionModifier] = useState('position-0')
 
-	const handleFilter = (filter) => {
+	const handleFilter = (filter, index) => {
+		// Move indicator
+		setPositionModifier('position-' + index)
+
+		// Filter projects
 		filterCallback(filter)
 	}
 
 	return (
 		<div className="RecentProjectsFilters">
-			<span className="RecentProjectsFilters__indicator" />
+			<span
+				className={
+					'RecentProjectsFilters__indicator ' +
+					(positionModifier ? positionModifier : '')
+				}
+			/>
 			{PROJECT_FILTERS.map((filter, index) => {
 				return (
 					<button
 						className="RecentProjectsFilters__btn f-os-regular-20-18 u-btn-clear active"
-						onClick={() => handleFilter(filter.slug)}
+						onClick={() => handleFilter(filter.slug, index)}
 						key={`filter-btn-${index}`}
 					>
 						{filter.name}
