c4b2e020e7d7f37f7604984f0c4389d8175b4d0d
Ollie Stevenson <ollie@stevenson.digital>
2020-06-10 21:25:09 +0100

    [add] new case study images

--- src/constants/caseStudySlider.js
+++ src/constants/caseStudySlider.js
@@ -1,26 +1,20 @@
 export const SLIDES = [
 	{
 		img: '/img/dashboard01@2x.png',
-		heading: 'Project 1 title here',
-		desc: 'Short project description goes here To explain what the project is all about.',
-		link: '/',
-	},
-	{
-		img: '/img/dashboard01@2x.png',
-		heading: 'Tech Test 5874',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
-		link: '/',
-	},
-	{
-		img: '/img/dashboard01@2x.png',
-		heading: 'Project 3 title here',
-		desc: 'Short project description goes here To explain what the project is all about.',
-		link: '/',
-	},
-	{
-		img: '/img/dashboard01@2x.png',
 		heading: 'Frontend Web Developer',
+		desc: 'Short project description goes here To explain what the project is all about.',
+		link: '/',
+	},
+	{
+		img: '/img/dashboard02@2x.png',
+		heading: 'Project Title',
 		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.',
+		link: '/',
+	},
+	{
+		img: '/img/dashboard03@2x.png',
+		heading: 'Last Case Study',
+		desc: 'Short project description goes here To explain what the project is all about.',
 		link: '/',
 	},
 ]
